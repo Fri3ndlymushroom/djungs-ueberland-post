4830030991d0036e155c71b0a69a40e33cc949e2
Fri3ndlymushroom <61848398+Fri3ndlymushroom@users.noreply.github.com>
2021-06-21 12:31:39 +0200

    html

---
+++ public/js/firebase.js
@@ -0,0 +1,36 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyC56ffd9pnKRokUhUA0NlTUGPVJwrLqJzg",
+    authDomain: "boys-chat-4ac68.firebaseapp.com",
+    projectId: "boys-chat-4ac68",
+    storageBucket: "boys-chat-4ac68.appspot.com",
+    messagingSenderId: "430083788605",
+    appId: "1:430083788605:web:4cdc01ebfd4a9beb04ff54",
+    measurementId: "G-7ZGXY9E7ZL"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+
+
+
+const auth = firebase.auth();
+const db = firebase.firestore();
+const storage = firebase.storage();
+const pathReference = storage.ref('images/stars.jpg');
+
+var gsReference = storage.refFromURL('gs://bucket/images/stars.jpg');
+
+if (location.hostname === 'localhost') {
+    db.useEmulator('localhost', 8080);
+    auth.useEmulator('http://localhost:9099/', { disableWarnings: true });
+    firebase.functions().useEmulator("localhost", 5001);
+}
+
+// lsof -t -i tcp:9099 | xargs kill & lsof -t -i tcp:5001 | xargs kill & lsof -t -i tcp:8080 | xargs kill & lsof -t -i tcp:5000 | xargs kill
+
+
+// netstat -aon | findstr :8080
+
+
+// taskkill /PID 18688 /F
