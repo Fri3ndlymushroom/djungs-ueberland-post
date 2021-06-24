63d65e0bfbf17e721b87384064f60d86b6daaaeb
Fri3ndlymushroom <61848398+Fri3ndlymushroom@users.noreply.github.com>
2021-06-24 10:53:51 +0200

    filterOutLink function

--- functions/index.js
+++ functions/index.js
@@ -9,16 +9,27 @@ exports.sendMessage = functions.https.onCall((data, context) => {
     let dateParts = String(admin.firestore.Timestamp.now().toDate()).split(" ")
     let date = dateParts[1] + " " + dateParts[2] + " " + dateParts[4].split(":")[0] + ":" + dateParts[4].split(":")[1]
 
+    let message = filterOutLinks(data.message)
+
+
+
+
     db.collection("messages").doc("messages").update({
         messages: admin.firestore.FieldValue.arrayUnion({
             username: data.username,
             date: date,
-            message: data.message,
+            message: message,
             uid: context.auth.uid,
         }),
     })
 
 })
+
+function filterOutLinks(message) {
+    return message
+}
+
+
 
 
 exports.newUserSignup = functions.auth.user().onCreate(async(user) => {
